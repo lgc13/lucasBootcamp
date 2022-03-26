8ff59c6fb6e014dccf80aefa8848f8a9453629f5
Lucas C <lucas.gomes.costa@gmail.com>
2022-03-25 21:34:42 -0400

    changed boolean names

--- js/coding_challenges/todo/script/todo.js
+++ js/coding_challenges/todo/script/todo.js
@@ -1,7 +1,7 @@
 const todoItems = [
-    { id: 1, text: "Take out trash and recycling", complete: true},
-    { id: 2, text: "Pick up dry cleaning", complete: false},
-    { id: 3, text: "Get oil change", complete: false},
-    { id: 4, text: "Write thank-you notes", complete: false},
+    { id: 1, text: "Take out trash and recycling", completed: true},
+    { id: 2, text: "Pick up dry cleaning", completed: false},
+    { id: 3, text: "Get oil change", completed: false},
+    { id: 4, text: "Write thank-you notes", completed: false},
 ];
 console.log("todoItems: ", todoItems)

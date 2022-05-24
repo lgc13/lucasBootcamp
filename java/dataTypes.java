6220f60f37a427aaa5ab4d77fc9984aea7ec2394
Lucas C <lucas.gomes.costa@gmail.com>
2022-05-23 21:12:07 -0400

    my first java file

---
+++ java/dataTypes.java
@@ -0,0 +1,12 @@
+// everything must be inside a class!
+class DataTypes {
+
+    // understand to the best of capabilities what all these words in the method mean
+    public static void main(String[] args) {
+        System.out.println("Hello world!!!!!!!!");
+
+        // how to create an sysout all the main data types we learned in TS
+        // string, number, boolean, null, undefined, array, object
+    }
+
+}

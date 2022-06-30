01ee3516d5967a382010e496e1a2e3ecf05b917d
Lucas C <lucas.gomes.costa@gmail.com>
2022-06-29 20:21:43 -0400

    learned how to use lombok

--- java/firstSpringBootService/src/main/java/com/my/firstSpringBootService/FirstSpringBootServiceApplication.java
+++ java/firstSpringBootService/src/main/java/com/my/firstSpringBootService/FirstSpringBootServiceApplication.java
@@ -11,6 +11,19 @@ public class FirstSpringBootServiceApplication {
 
 		System.out.println("This is a test");
 		sayHello();
+
+		Car myCar2 = new Car();
+		System.out.println("myCar2: " + myCar2);
+
+		Car myCar = new Car("Elantra", "Hyundai", 2013, "Black", 4);
+		System.out.println("myCar: " + myCar);
+
+		String myCarMake = myCar.getMake();
+		System.out.println("myCarMake: " + myCarMake);
+
+		myCar.setColor("Hot Red");
+		System.out.println("myCar color: " + myCar.getColor());
+
 	}
 
 	private static void sayHello() {

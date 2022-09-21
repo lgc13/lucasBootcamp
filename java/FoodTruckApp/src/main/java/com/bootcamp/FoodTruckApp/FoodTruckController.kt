d59fb8dcb20afce424e06827c86fba4621d13a17
Lucas C <lucas.gomes.costa@gmail.com>
2022-09-20 20:01:50 -0400

    added intiial food truck app stuff

---
+++ java/FoodTruckApp/src/main/java/com/bootcamp/FoodTruckApp/FoodTruckController.kt
@@ -0,0 +1,9 @@
+package com.bootcamp.FoodTruckApp
+
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class FoodTruckController {
+
+
+}
---
+++ java/FoodTruckApp/src/test/java/com/bootcamp/FoodTruckApp/FoodTruckAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.FoodTruckApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodTruckAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
